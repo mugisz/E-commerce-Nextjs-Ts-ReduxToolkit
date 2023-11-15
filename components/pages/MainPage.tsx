4a92b164e375a4cb67e8b5e6390e5ae45b000837
mugisz <danyasarabura@gmail.com>
2023-11-15 23:01:09 +0200

    Add grab redeveloped category

--- components/pages/MainPage.tsx
+++ components/pages/MainPage.tsx
@@ -1,6 +1,48 @@
 import Image from "next/image";
 import style from "../pages/main.module.scss";
 export default function MainPage() {
+  const categoryMas = [
+    {
+      img: "https://content2.rozetka.com.ua/goods/images/big_tile/364623745.jpg",
+      name: "Iphone",
+    },
+    {
+      img: "https://content.rozetka.com.ua/goods/images/big/341858430.jpg",
+      name: "MackBook",
+    },
+    {
+      img: "https://content.rozetka.com.ua/goods/images/big_tile/224009738.jpg",
+      name: "Ipad",
+    },
+  ];
+  const popularItemMas = [
+    {
+      img: "https://content2.rozetka.com.ua/goods/images/big_tile/364623745.jpg",
+      name: "Iphone 15 Pro 128",
+      price: 29999,
+    },
+    {
+      img: "https://content.rozetka.com.ua/goods/images/big_tile/224009738.jpg",
+      name: "Ipad 4 mini",
+      price: 16550,
+    },
+    {
+      img: " https://content.rozetka.com.ua/goods/images/big_tile/284913550.jpg",
+      name: "Iphone 14 128gb",
+      price: 20000,
+    },
+    {
+      img: "https://content2.rozetka.com.ua/goods/images/big_tile/269256826.jpg",
+      name: "MackBook Air m2",
+      price: 40000,
+    },
+    {
+      img: "https://content.rozetka.com.ua/goods/images/big_tile/341858430.jpg",
+      name: "MackBook Pro m2",
+      price: 72000,
+    },
+  ];
+
   return (
     <main className={style.main}>
       <section className={style.topSection}>
@@ -9,26 +51,38 @@ export default function MainPage() {
           <span className={style.spanColor}> Iphones</span>
         </h1>
         <div className={style.cardFlex}>
-          <div className={style.cardItem}>
-            <Image
-              alt="iphone"
-              src="https://content2.rozetka.com.ua/goods/images/big_tile/364623745.jpg"
-              width={100}
-              height={175}
-            />
-            <p className={style.phoneName}>Iphone 15 Pro</p>
-            <div className={style.priceBlock}>
-              <span className={style.price}>₴ 32334</span>
-              <strike className={style.grab}> ₴ 5000</strike>
+          {popularItemMas?.map((mas) => (
+            <div className={style.cardItem}>
+              <div className={style.imageBlock}>
+                <Image alt="iphone" src={mas.img} width={100} height={125} />
+              </div>
+              <p className={style.phoneName}>{mas.name}</p>
+              <div className={style.priceBlock}>
+                <span className={style.price}>₴ {mas.price}</span>
+                <strike className={style.grab}>₴ {mas.price + 2400}</strike>
+              </div>
+              <div className={style.absoluteRightBLock}>
+                <p>
+                  56%
+                  <br />
+                  OFF
+                </p>
+              </div>
             </div>
-            <div className={style.absoluteRightBLock}>
-              <p>
-                56%
-                <br />
-                OFF
-              </p>
+          ))}
+        </div>
+      </section>
+      <section className={style.gadgetCatrgory}>
+        <h1 className={style.topTitle}>
+          <span className={style.spanColor}>Gadget</span> Category
+        </h1>
+        <div className={style.flexCategory}>
+          {categoryMas?.map((mas) => (
+            <div className={style.categoryItem}>
+              <Image width={100} height={125} src={mas.img} />
+              <p className={style.categoryName}>{mas.name}</p>
             </div>
-          </div>
+          ))}
         </div>
       </section>
     </main>
